1e40d5b691dd0cdf291215243f55ae6e825c3e3b
jiseok01 <126743121+jiseok01@users.noreply.github.com>
2023-05-16 11:19:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
 st.title("this is the app title")
+
+dp = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=['x', 'y'])
+st.line_chart(df)
